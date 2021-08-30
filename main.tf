f6b3a0d73d4d5fbff481d871e1294de19854bc8b
KiranKumarvuke <kiran.4b8@outlook.com>
2021-08-30 22:37:52 +0530

    added lfs files

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.63.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  #  use_msi = true
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9598831da59e2c704bb6f7581d77a494e72fccae453f27ea32ffa0046bdb150
+size 193
--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,4 +1,3 @@
-resource "azurerm_resource_group" "tlz_resource_group" {
-  name     = "demorg"
-  location = "centralus"
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9f957820002ffa5a24538817c78c436f839cac5e448d35a932c2838e04d6876b
+size 110
